3f129a1414516ef365d0fbb4d2632a669a46e823
Eve <948832626@qq.com>
2020-06-01 23:51:02 +0800

    feat: 新增'盛最多水的容器'

---
+++ questions/11. 『中等』盛最多水的容器/index.ts
@@ -0,0 +1,31 @@
+const { printDemo } = require('../../tool/console.ts');
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const maxArea = (arr: number[]): number => {
+  let max = 0;
+  // 循环计算当前距离内所能得到的最大值
+  for (let distance = 1; distance < arr.length; distance++) {
+    let left = 0;
+    let right = distance;
+    while (right < arr.length) {
+      const min = Math.min(arr[left], arr[right]);
+      const area = distance * min;
+      max = area > max ? area : max;
+      left++;
+      right++;
+    }
+  }
+  return max;
+};
+
+const data = [
+  { input: [[1, 8, 6, 2, 5, 4, 8, 3, 7]], expect: 49 },
+  { input: [[1, 1]], expect: 1 }
+];
+
+printDemo(maxArea, data);
+
+export {};
