6c7fcfc5d1e3a9e9ff8dffef67fbf210fa0066e1
Eve <948832626@qq.com>
2020-06-11 19:07:18 +0800

    refactor: 使用set解决一道题

---
+++ questions/3. 『中等』无重复字符的最长子串/index2.ts
@@ -0,0 +1,43 @@
+const { printDemo } = require('../../tool/console.ts');
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = (s: string): number => {
+  if (s.length === 0) {
+    return 0;
+  }
+  const set = new Set<string>();
+  const arr = s.split(''); // 将字符串转为字符串数组
+  let max = 0;
+  arr.forEach((v) => {
+    if (set.has(v)) {
+      for (const iterator of set.values()) {
+        if (set.has(v)) {
+          set.delete(iterator);
+        } else {
+          break;
+        }
+      }
+      set.add(v);
+    } else {
+      set.add(v);
+    }
+    max = Math.max(max, set.size);
+  });
+  return max;
+};
+
+const data = [
+  { input: ['abcabcbb'], expect: 3 },
+  { input: ['bbbbb'], expect: 1 },
+  { input: ['pwwkew'], expect: 3 },
+  { input: ['aab'], expect: 2 },
+  { input: ['qrsvbspk'], expect: 5 },
+  { input: ['dvdf'], expect: 3 }
+];
+
+printDemo(lengthOfLongestSubstring, data);
+
+export {};
